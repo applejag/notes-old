b30ab7250363fa915bff2de99b1b1077e5f0ccae
Kalle Jillheden <kalle.jillheden@gmail.com>
2021-01-23 18:26:10 +0100

    Added script to rename zettels

---
+++ mv_zettel.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+ZETTEL="${1:?Zettel file must be provided}"
+NEW_NAME="${2:?Zettel new name must be provided}"
+
+if [[ ! -f "$ZETTEL.md" ]]; then
+    echo "File '$ZETTEL.md' does not exist." >&2
+    exit 1
+fi
+
+if [[ -f "$NEW_NAME.md" ]]; then
+    echo "File '$NEW_NAME.md' already exist." >&2
+    exit 1
+fi
+
+mv "$ZETTEL.md" "$NEW_NAME.md"
+sed -i "s/$ZETTEL/$NEW_NAME/g" ./*.md
